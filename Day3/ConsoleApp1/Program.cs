8e135b5d7aa816dd6f1aedf59e576208fcbca66a
rupa2997 <65192838+rupa2997@users.noreply.github.com>
2020-12-03 09:58:33 +0530

    day 2 and day 1 assignments

---
+++ Day3/ConsoleApp1/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager m1 = new Manager();
+            Manager m2 = new Manager("rupali",2,300000,"CEO");
+            Console.WriteLine(m2.empNo);
+            Console.WriteLine(m2.empName);
+            Console.WriteLine(m2.deptNo);
+            Console.WriteLine(m2.Basic);
+            Console.WriteLine(m2.CalcNetSalary());
+            Console.WriteLine("==============================");
+            Console.WriteLine("GM");
+            GeneralManager g1=new GeneralManager("sonali",3,400000,"CMO","dddd");
+            Console.WriteLine(g1.empName);
+            Console.WriteLine(g1.deptNo);
+            Console.WriteLine(g1.Basic);
+            Console.WriteLine(g1.CalcNetSalary());
+            Console.WriteLine("===ceo===============");
+            CEO c1=new CEO("Pallavi",6,80000);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+    public abstract class employee {
+        public employee() {
+            count++;
+            empNo = count;
+            empName = "rupali";
+            deptNo = 1;
+            Basic = 20000;
+
+        }
+                public employee(string empName, short deptNo, decimal Basic) {
+            Console.WriteLine("employee 3 para cons");
+            count++;
+            empNo = count;
+            this.empName = empName;
+            this.deptNo = deptNo;
+            this.Basic = Basic;
+
+        }
+
+        public static int count = 0;
+        public int empNo;
+        public int emp_No {
+            set; get;
+        }
+        public string empName;
+        public string emp_Name{
+            set {
+                if (value == "")
+                    Console.WriteLine("invalid name");
+                else
+                    empName = value;
+            }
+            get {
+                return empName;
+            }
+
+        }
+
+        public short deptNo;
+        public short dept_No {
+            set {
+                if (value > 0)
+                    deptNo = value;
+                else
+                    Console.WriteLine("invalid dept number");
+            }
+            get {
+                return deptNo;
+            }
+        }
+
+        public  decimal Basic;
+        public abstract decimal BasicS {
+            set;get;
+        }
+
+        public int calnetsalary;
+        public abstract decimal CalcNetSalary();
+
+    }
+    public class Manager : employee {
+
+        public  Manager()
+        {
+            Console.WriteLine("manager 0 para");
+            count++;
+            empNo = count;
+            empName = "rupali";
+            deptNo = 1;
+            Basic = 20000;
+            designation="CMO";
+        }
+        public Manager(string empName,short deptNo, decimal Basic,string  designation):base(empName, deptNo, Basic)
+        {
+            Console.WriteLine("Manager 3 para");
+            //count++;
+            //empNo = count;
+            //this.empName = empName;
+            //this.deptNo = deptNo;
+            //this.Basic = Basic;
+            this.designation=designation;
+        }
+        public string designation;
+        public string Designation {
+
+            set {
+                if (value == "")
+                {
+                    Console.WriteLine("invalid designation");
+                }
+                else {
+                    designation = value;
+                }
+            }
+            get
+            {
+                return designation;
+            }
+        }
+
+        public override decimal BasicS
+        {
+            set {
+                if (value > 50000)
+                    Basic = value;
+            }
+            get {
+
+                return Basic;
+            }
+        }
+        public  override decimal CalcNetSalary() {
+            
+            
+            return (Basic-((15*Basic)/100));
+        }
+    }
+    public class GeneralManager : Manager {
+        public GeneralManager()
+        {
+            Console.WriteLine("General 0 para cons");
+        }
+        public GeneralManager( string empName, short deptNo, decimal Basic,string  designation,string perks):base(empName, deptNo, Basic, designation)
+        {
+            Console.WriteLine("general 4 para cons");
+            this.perks = perks;
+        }
+        public string perks;
+        public string Perks
+        {
+            set {
+                perks = value;
+            }
+            get
+            {
+                return perks;
+            }
+        }
+         public  override decimal CalcNetSalary() {
+            return  (Basic-((15*Basic)/100));
+        }
+    }
+    public class  CEO : employee
+    {
+        public CEO(string empName, short deptNo, decimal Basic):base(empName, deptNo, Basic)
+        {
+            Console.WriteLine("ceo 4 para cons");
+            
+        }
+        public override decimal BasicS
+        {
+            set
+            {
+                if (value > 50000)
+                    Basic = value;
+            }
+            get
+            {
+
+                return Basic;
+            }
+        }
+
+        public sealed override decimal CalcNetSalary()
+        {
+            return (Basic-((30*Basic)/100));
+        }
+
+    }
+}
---
+++ day_2/ass1/ass1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee("sonali",20000,1);
+            Employee e2 = new Employee("rajesh", 2000);
+            Employee e3 = new Employee("pallavi");
+            Employee e4 = new Employee();
+
+            Console.WriteLine(e1.Emp_No);
+            Console.WriteLine(e1.Emp_Name);
+            Console.WriteLine(e1.empSalary);
+            Console.WriteLine(e1.dept_no);
+            e1.netSalary();
+            Console.WriteLine(e1.Emp_No);
+            Console.WriteLine(e2.Emp_No);
+            Console.WriteLine(e3.Emp_No);
+            Console.WriteLine(e4.Emp_No);
+            Console.WriteLine(e3.Emp_No);
+            Console.WriteLine(e2.Emp_No);
+            Console.WriteLine(e1.Emp_No);
+        }
+    }
+    class Employee
+    {
+        public Employee(){
+
+            count++;
+            Emp_No = count;
+            Emp_Name = "Rupali";
+            empSalary = 20000;
+            dept_no = 1;
+
+        }
+        public Employee(string Emp_Name,int empSalary,int dept_no)
+        {
+            count++;
+            Emp_No = count;
+            this.Emp_Name = Emp_Name;
+            this.empSalary = empSalary;
+            this.dept_no = dept_no;
+
+        }
+        public Employee(string Emp_Name, int empSalary)
+        {
+            count++;
+            Emp_No = count;
+            this.Emp_Name = Emp_Name;
+            this.empSalary = empSalary;
+           
+
+        }
+
+        public Employee(string Emp_Name)
+        {
+            count++;
+            Emp_No = count;
+            this.Emp_Name = Emp_Name;
+          
+        }
+        private int emp_No;
+        public int Emp_No {
+            set;
+            get;
+        }
+        public static int count = 0;
+        private string empName;
+        public string Emp_Name {
+            set {
+                if (value == "") {
+                    Console.WriteLine("Name cannot Be blank");
+                }
+                else
+                {
+                    empName = value;
+                }
+            }
+            get {
+                return empName;
+            }
+        }
+        
+        private int salary;
+        public int empSalary {
+            set {
+                if (value > 10000 && value < 100000)
+                {
+                    salary = value;
+                }
+                else {
+                    Console.WriteLine("Invalid Salary");
+                }
+            }
+            get {
+                return salary;
+            }
+        }
+        private int deptNo;
+        public int dept_no {
+            set {
+                if (value > 0)
+                {
+                    deptNo = value;
+                }
+                else {
+                    Console.WriteLine("Invalid Department Number");
+                }
+            }
+            get {
+                return deptNo;
+            }
+        }
+       
+        public void netSalary() {
+            Console.WriteLine("Net Salary Is"+(empSalary-((10/100)*empSalary)));
+        }
+    }
+}
